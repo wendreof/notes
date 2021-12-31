14f6c0d5ce52aa7428cb13e8548f2a6cbb1e87ce
Wendreo Fernandes <wendreo@outlook.com>
2021-12-31 02:15:21 -0300

    modified: Added beta deploy + firebase configuration

--- git_hooks/commitMessage.dart
+++ git_hooks/commitMessage.dart
@@ -6,7 +6,7 @@ dynamic main() {
   final commitMessage = commitFile.readAsStringSync();
 
   final regExp = RegExp(
-    r'(fix|feat|wip|none|chore|refactor|docs|style|test)\:.+',
+    r'(add|modified|fix|feat|wip|none|chore|refactor|docs|style|test)\:.+',
   );
 
   final valid = regExp.hasMatch(commitMessage);
